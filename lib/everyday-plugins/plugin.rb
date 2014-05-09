3562d28ce195a01ab7a0a474756edcc66cee30f8
Eric Henderson <henderea@gmail.com>
2014-05-09 13:19:06 -0400

    update dependencies and add the TypeHelper that I've been putting in individual projects

--- lib/everyday-plugins/plugin.rb
+++ lib/everyday-plugins/plugin.rb
@@ -1,3 +1,7 @@
+require 'everyday-cli-utils'
+include EverydayCliUtils
+import :maputil
+
 module EverydayPlugins
   class Plugins
     def self.instance
@@ -111,6 +115,53 @@ module EverydayPlugins
 
     def register_variable(name, value = nil)
       EverydayPlugins::Plugins.instance.register_variable(name, value)
+    end
+  end
+  module TypeHelper
+    def basic_type(list, *args)
+      options = Plugins.get_var :options
+      list.any? { |item|
+        if item[:block].nil?
+          flag_boolean(item, options)
+        else
+          item[:block].call(options, *args)
+        end
+      }
+    end
+
+    def flag_boolean(item, options)
+      item[:options].has_key?(:option) && options[item[:options][:option]] == (item[:options].has_key?(:value) ? item[:options][:value] : true)
+    end
+
+    def complex_filter(list, options, symbol)
+      list.filtermap { |item|
+        if item[:block].nil?
+          if item[:options].has_key?(symbol) && flag_boolean(item, options)
+            item[:options][symbol]
+          else
+            item[:options].has_key?(:option) && !options[item[:options][:option]].nil? ? options[item[:options][:option]] : false
+          end
+        else
+          rval = item[:block].call(options)
+          (rval.nil? || !rval) ? false : rval
+        end
+      }
+    end
+
+    def simple_type(list, *args)
+      options = Plugins.get_var :options
+      list.sort_by { |v| v[:options][:order] }.each { |item| item[:block].call(options, *args) }
+    end
+
+    def simple_type_with_result(list)
+      result = Plugins.get_var :result
+      simple_type(list, result)
+    end
+
+    def get_name(list)
+      options = Plugins.get_var :options
+      rval    = complex_filter(list.sort_by { |v| -v[:options][:priority] }, options, :name)
+      (rval.nil? || rval.empty?) ? false : rval.first
     end
   end
 end
